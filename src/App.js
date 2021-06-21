26de73a3ba49a915ad5205ffcbf48624a6e90d8f
unknown <oumaymamesbahi2@gmail.com>
2021-06-21 10:48:35 +0200

    first-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import téléchargement from './imgs/téléchargement.jpg'
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    function App() {
+      return (
+        <>
+        <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+        <h1 className="titlered">Your name here</h1>
+        <br />
+        <img  src="/img/nature1.JPG" />
+        <br />
+        <img src={téléchargement} />
+      </div>
+      <video width={320} height={240} controls>
+        <source src="myVideo.mp4" type="video/mp4" />
+      </video>
+      <p>Enter your HTML here</p>
+    </>)
+   
+    ;
+
+
 }
 
 export default App;
